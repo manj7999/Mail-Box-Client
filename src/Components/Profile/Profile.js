97ed29999a944da45c2a37b0c2a1095dac995bde
Ankit Manjhi <ankitmanjhi286@gmail.com>
2024-03-10 20:32:23 +0530

    Mail Box Client

---
+++ src/Components/Profile/Profile.js
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from './Profile.module.css';
+
+const Profile = () => {
+  return (
+    <section className={classes.welcome}>
+      <h1>Welcome to Metro Mail</h1>
+    </section>
+  );
+};
+
+export default Profile;
